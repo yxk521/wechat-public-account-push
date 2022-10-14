aa43fba85f4deb3760340ef781d9a241c5c44eb6
yxk521 <65714825+yxk521@users.noreply.github.com>
2022-10-14 16:05:44 +0800

    Update index.cjs

--- config/index.cjs
+++ config/index.cjs
@@ -21,7 +21,7 @@ USE_PASSAGE: 'push-deer',
       // 想要发送的人的名字
       name: '宝贝',
       // 使用微信测试号：扫码关注你的微信测试号后生成的一段字符串，在测试号后台能看到
-      id: 'PDU16911Ts9GjLRc53QHMiItYWQFLXgGcYhXaqdmv',
+      id: 'oVnMI6LdeHeMPlzUE8IIVjIp6JX0',
       // 使用微信测试号：你想对他发送的模板消息的模板ID
       useTemplateId: 'dH-1VT7SK3hXqxtrGzIvRvotzp8QxPI5R5DMfSacKO8',
       // 新历生日, 仅用作获取星座运势, 格式必须为MM-DD
