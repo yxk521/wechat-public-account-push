0c569677855ccca775d06712e4026d34c5d880bd
yxk521 <65714825+yxk521@users.noreply.github.com>
2022-10-14 15:54:14 +0800

    Update index.cjs

--- config/index.cjs
+++ config/index.cjs
@@ -8,10 +8,10 @@
 const USER_CONFIG = {
 USE_PASSAGE: 'push-deer',
   // 使用微信测试号：公众号APP_ID
-  APP_ID: '',
+  APP_ID: 'wxb101cf6bdb080145',
 
   // 使用微信测试号：公众号APP_SECRET
-  APP_SECRET: '',
+  APP_SECRET: 'fbd4fb13071b3a865198630776dd9dbd',
 
   PROVINCE: '广东',
   CITY: '惠州',
@@ -23,7 +23,7 @@ USE_PASSAGE: 'push-deer',
       // 使用微信测试号：扫码关注你的微信测试号后生成的一段字符串，在测试号后台能看到
       id: 'PDU16911Ts9GjLRc53QHMiItYWQFLXgGcYhXaqdmv',
       // 使用微信测试号：你想对他发送的模板消息的模板ID
-      useTemplateId: '',
+      useTemplateId: 'dH-1VT7SK3hXqxtrGzIvRvotzp8QxPI5R5DMfSacKO8',
       // 新历生日, 仅用作获取星座运势, 格式必须为MM-DD
       horoscopeDate: '12-27',
       festivals: [
@@ -51,13 +51,13 @@ USE_PASSAGE: 'push-deer',
 
 
   // 【推送完成提醒】模板id, 用来看自己有没有发送成功的那个模板
-  CALLBACK_TEMPLATE_ID: '',
+  CALLBACK_TEMPLATE_ID: '1D5QakvSZ7ogGc3fMZBQuDOmNT7vA_GxhplDi0d-CnY',
 
   CALLBACK_USERS: [
     {
       name: '自己',
       // 使用微信测试号：自己的微信id，扫码关注你的微信测试号后生成的一段字符串，在测试号后台能看到
-      id: '',
+      id: 'oVnMI6LdeHeMPlzUE8IIVjIp6JX0',
     }
   ],
 
